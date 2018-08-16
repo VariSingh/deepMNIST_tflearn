b32e97c0bc58bacccc4d5db879278ef08c15466f
Varinder Singh <varindersingh271990@gmail.com>
2018-08-16 23:56:08 +0530

    wrote program for convolution neural network

--- mnist.py
+++ mnist.py
@@ -1,24 +1,35 @@
 import tflearn
-
+from tflearn.layers.conv import conv_2d, max_pool_2d
 #load data
 import tflearn.datasets.mnist as mnist
 X, Y, testX, testY = mnist.load_data(one_hot=True)
 
-#neural network
-input = tflearn.input_data(shape=[None,784])
-input = tflearn.fully_connected(input,100,activation='relu')
-
-layer1 = tflearn.fully_connected(input,100,activation='relu')
-
-layer2 = tflearn.fully_connected(layer1,100,activation='relu')
-
-output = tflearn.fully_connected(layer2,10,activation='softmax')
+X = X.reshape([-1,28,28,1])
+testX = testX.reshape([-1,28,28,1])
 
 
-sgd = tflearn.SGD(learning_rate=0.1)
-top_k = tflearn.metrics.Top_k(3)
-net = tflearn.regression(output, optimizer=sgd, metric=top_k, loss='categorical_crossentropy')
 
-#training
-model = tflearn.DNN(net,tensorboard_verbose=0)
-model.fit(X,Y, n_epoch=20, validation_set=(testX,testY),show_metric=True, run_id="dense_model")
+#Convolutional neural network
+
+
+input = tflearn.input_data(shape=[None,28,28,1],name="input")
+
+conv1 = conv_2d(input,32,3, activation="relu", regularizer="l2")
+
+max_pool1 = max_pool_2d(conv1,2)
+
+conv2 = conv_2d(max_pool1,64,3, activation="relu", regularizer="l2")
+
+max_pool2 = max_pool_2d(conv2,2)
+
+fully_connected1 = fully_connected(max_pool2,100,activation="relu")
+
+fully_connected2 = fully_connected(fully_connected1,10,activation="softmax")
+
+regression = regression(fully_connected2, optimizer="adam", learning_rate=0.01, loss="categorical_crossentropy", name="target")
+
+
+#Training
+
+model = tflearn.DNN(regression,tensorboard_verbose=0)
+model.fit(X,Y, n_epoch=20, validation_set=(testX,testY),show_metric=True, run_id="convNet_model")
